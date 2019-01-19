85c05d5b38b862fcf45e3cd5e92b3508998cacf3
joao <j.batistareis@gmail.com>
2019-01-19 20:19:17 -0300

    added stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { ShortenedUrl } from './shortenedUrl'
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { HttpParams } from "@angular/common/http";
 import * as moment from 'moment';
+
+import { ShortenedUrl } from './shortenedUrl'
+import { Stats } from './stats'
 
 @Component({
   selector: 'app-root',
@@ -14,9 +16,20 @@ export class AppComponent {
 
   public orginalUrl: string = undefined;
   public shortenedUrl: ShortenedUrl = undefined;
+  public stats: Stats = undefined;
   public errorMessage: string = undefined;
 
   constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get("https://ushort-api.herokuapp.com/api/stats").subscribe(
+      (response) => this.stats = response as Stats,
+      (error) => {
+        this.errorMessage = error.message;
+        console.log(error);
+      }
+    );
+  }
 
   closeErrorModal() {
     this.errorMessage = undefined;
---
+++ src/app/stats.ts
@@ -0,0 +1,7 @@
+export class Stats {
+    constructor(
+        public reducedUrls: number,
+        public reducedLeters: number,
+        public averageUrlSize: number
+    ) { }
+}
