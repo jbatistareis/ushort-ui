e1cd4b195ce29df2351446ad67832f04b4e1976a
joao <j.batistareis@gmail.com>
2019-01-19 21:11:10 -0300

    added admin panel, separated home from main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminPanelComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPanelComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpParams } from "@angular/common/http";
+import * as moment from 'moment';
+
+import { ShortenedUrl } from './shortenedUrl'
+import { Stats } from './stats'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public orginalUrl: string = undefined;
+  public shortenedUrl: ShortenedUrl = undefined;
+  public stats: Stats = undefined;
+  public errorMessage: string = undefined;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get("https://ushort-api.herokuapp.com/api/stats").subscribe(
+      (response) => this.stats = response as Stats,
+      (error) => {
+        this.errorMessage = error.message;
+        console.log(error);
+      }
+    );
+  }
+
+  closeErrorModal() {
+    this.errorMessage = undefined;
+  }
+
+  shortenUrl() {
+    if (this.orginalUrl) {
+      let params = new HttpParams().set('url', this.orginalUrl);
+      this.httpClient.get("https://ushort-api.herokuapp.com/api/shorten", { params }).subscribe(
+        (response) => this.shortenedUrl = response as ShortenedUrl,
+        (error) => {
+          this.shortenedUrl = undefined;
+          this.errorMessage = error.message;
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  parseDate(date: string) {
+    return moment(date).format('MM/DD/YYYY HH:mm:ss');
+  }
+
+}
